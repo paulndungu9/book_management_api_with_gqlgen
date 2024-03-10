20bc09d99f957ea9e3fc7435bb3cfc55d59111f1
paulndungu9 <paunjeri12@gmail.com>
2024-03-10 15:36:24 +0300

    set up graphql schema and generate resolvers

--- graph/model/models_gen.go
+++ graph/model/models_gen.go
@@ -2,25 +2,16 @@
 
 package model
 
+type Book struct {
+	ID              string `json:"id"`
+	Title           string `json:"title"`
+	Author          string `json:"author"`
+	PublicationYear int    `json:"publication_year"`
+	Genre           string `json:"genre"`
+}
+
 type Mutation struct {
 }
 
-type NewTodo struct {
-	Text   string `json:"text"`
-	UserID string `json:"userId"`
-}
-
 type Query struct {
-}
-
-type Todo struct {
-	ID   string `json:"id"`
-	Text string `json:"text"`
-	Done bool   `json:"done"`
-	User *User  `json:"user"`
-}
-
-type User struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
 }
