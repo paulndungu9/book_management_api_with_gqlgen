121510f2c2872931e3a6396b9645bf25c75841b4
paulndungu9 <paunjeri12@gmail.com>
2024-03-10 18:43:58 +0300

    connect database,makefile and initial migrates

--- server.go
+++ server.go
@@ -1,6 +1,14 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/pgdialect"
+	"github.com/uptrace/bun/driver/pgdriver"
+	"github.com/uptrace/bun/extra/bundebug"
+
 	"log"
 	"net/http"
 	"os"
@@ -12,17 +20,60 @@ import (
 
 const defaultPort = "8080"
 
+var DB *bun.DB
+
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
+	err := connectToDatabase()
+	if err != nil {
+		log.Fatalf("Unable to connect to the database: %s", err.Error())
+	}
+	fmt.Println("Successfully connected to the database")
+	resolver := &graph.Resolver{
+		DB: DB,
+	}
 
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	es := graph.NewExecutableSchema(graph.Config{Resolvers: resolver})
+	srv := handler.NewDefaultServer(es)
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/graphql", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+func connectToDatabase() error {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	// Build the database connection string
+	dsn := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_SSL_MODE"),
+	)
+
+	// Open a connection to the PostgreSQL database
+	sqldb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(dsn)))
+
+	// Initialize the bun.DB instance with the PostgreSQL dialect
+	DB = bun.NewDB(sqldb, pgdialect.New())
+
+	// Add a query hook for debugging purposes
+	DB.AddQueryHook(bundebug.NewQueryHook(
+		bundebug.WithVerbose(true),
+		bundebug.FromEnv("BUNDEBUG"),
+	))
+
+	return DB.Ping()
 }
